2d967957952542f65f50445d9b0fcf57d1f54e1d
Ted Osiobe <osiobeted@gmail.com>
2022-09-08 18:05:09 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
-
-/*
- * main - main function is the entry point
- * Return: Always 0 (success)
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
 	puts("Programming is like building a multilingual puzzle\n");
-
 	return (0);
 }
