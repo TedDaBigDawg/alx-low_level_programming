eb17d0ec0818844024632d7f4377ef0698088698
Ted Osiobe <osiobeted@gmail.com>
2022-09-09 18:59:25 -0700

    3 alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char i;
+	char p;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		putchar(i);
+	}
+
+	for (p = 'A'; p <= 'Z'; p++)
+	{
+		putchar(p);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
