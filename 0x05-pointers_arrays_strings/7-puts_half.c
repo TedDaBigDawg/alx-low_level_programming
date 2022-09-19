4d988cd978187fa8a466abc52d30a864f8f1fa53
Ted Osiobe <osiobeted@gmail.com>
2022-09-19 08:16:42 -0700

    pointers

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * puts_half - prints the second half of a string
+ * @str: string to be used
+ *
+ * Return: nothing
+ */
+
+void puts_half(char *str)
+{
+	int i, j;
+
+	i = 0;
+
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+
+	if (i % 2 == 1)
+	{
+		j = i / 2;
+	}
+	else
+	{
+		j = (i - 1) / 2;
+	}
+
+	for (j++; j < i; j++)
+	{
+		_putchar(str[j];
+	}
+	_putchar('\n');
+}
