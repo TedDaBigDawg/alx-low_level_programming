bdb7e8298c5280930d3e6f3201ad055536d9c370
Ted Osiobe <osiobeted@gmail.com>
2022-10-04 03:17:59 -0700

    Static Libraries

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _isupper - check
+ *
+ * Return: something
+ */
+
+int _isupper(int c)
+{
+
+}
---
+++ 0x09-static_libraries/putchar.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _putchar - prints a character
+ *
+ * @c: character to print
+ * Return: something
+ */
+
+int _putchar(char c)
+{
+
+}
