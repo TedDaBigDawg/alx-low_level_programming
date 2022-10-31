8404985bf35cc73920ac45a74c3304685d08f941
Ted Osiobe <osiobeted@gmail.com>
2022-10-31 08:13:45 -0700

    File I/O

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -29,7 +29,7 @@ int create_file(const char *filename, char *text_content)
 		text_content = "";
 	}
 
-	for (nofl = 0; text_content[nofl]; nof++)
+	for (nofl = 0; text_content[nofl]; nofl++)
 		;
 
 	rw = write(fd, text_content, nofl);
